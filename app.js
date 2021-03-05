bd419ba03beb567b23b6850de5af63d6dfddedf1
timursaurus <bolotovtmr@gmail.com>
2021-03-05 12:14:04 +0600

    started auth

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use('/u/auth', require('./routes/auth.routes'))
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        server.listen(5000)
+    } catch (e) {
+        console.log('server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -13,5 +32,4 @@ app.get('/:room/', (req, res) => {
     res.render('room')
 })
 
-server.listen(5000)
 
---
+++ routes/auth.routes.js
@@ -0,0 +1,12 @@
+const {Router} = require('express')
+const router = Router()
+
+router.post('/signup', async (req, res) => {
+
+})
+
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
